e74b550552b1afc5189d16a25cc0526c99d19ced
sanan005 <sananmarakkar19@gmail.com>
2024-10-29 09:01:06 +0400

    simplesums-2
    This is the second exercise of the Assessment named simplesums
    
    this deals with adding integer value to variables and printing the desired sum of the two

---
+++ simplesums.py
@@ -0,0 +1,18 @@
+"""In this exercise, you will create and work with integer variables, 
+perform arithmetic operations, and print the result to the console."""
+
+#Declaring a variable and initializing it with the integer value 8.
+
+a=8
+
+#Declaring a second variable and initializing it with the integer value 10.
+
+b=10
+
+#Declaring a third variable that stores the sum of first two numbers.
+
+c=a+b
+
+#Printing the value of the sum to the console.
+
+print(c)
