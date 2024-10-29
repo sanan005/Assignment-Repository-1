38cc86883c4fe581444878d53bfca4674299eaa2
sanan005 <sananmarakkar19@gmail.com>
2024-10-29 19:44:33 +0400

    primitivequiz-4-Assignment
    This is 4rth Exercise of the First Assignment named primitive quiz
    
    this deals with the understanding of user input and use of if else statement .
    it also uses the conceptual understanding of using .lower() function to evaluate the user output to produce a defined result without any errors

---
+++ primitivequiz.py
@@ -0,0 +1,31 @@
+"""In this exercise, you'll create a simple program that asks the user a question, evaluates their answer, and provides feedback."""
+
+#write a program that asks the user "What is the capital of France?" and waits for their response.
+question=input("what is the capital of france=   ")
+
+#If the user's answer is correct (i.e., "Paris"), the program should print a message saying the answer is correct.
+if question.lower()=="paris":
+    print("The Answer Is Right!!!")
+#If the answer is incorrect, the program should print a message saying the answer is wrong
+else:
+    print("your answer in incorrect")
+
+#Advanced Requirements
+"""Ignore Capitalization: Modify your program to accept answers regardless of the capitalization (e.g., "paris", "Paris", and "PaRis" should all be considered correct). 
+Multiple Questions: Extend the program into a quiz that asks for the capitals of 10 European countries. 
+Provide feedback for each question."""
+
+capital1=input("enter the capital of INDIA")
+capital2=input("enter the capital of UAE")
+capital3=input("enter the capital of USA")
+capital4=input("enter the capital of CANADA")
+capital5=input("enter the capital of UK")
+capital6=input("enter the capital of QATAR")
+capital7=input("enter the capital of SAUDI ARABIA")
+capital8=input("enter the capital of BAHRAIN")
+capital9=input("enter the capital of PALESTIENE")
+capital10=input("enter the capital of EGYPT")
+
+
+
+
